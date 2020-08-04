3486a1240e94a2f0f398805a28fd9f1f37c4d569
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-08-04 21:55:27 +0200

    Added graph retrieval

---
+++ retrieve_graphs.py
@@ -0,0 +1,7 @@
+import compress_json
+from encodeproject import download
+from tqdm.auto import tqdm
+
+data = compress_json.load("graphs.json")
+for url in tqdm(data.values(), desc="Downloading graphs"):
+    download(url, path=path)
