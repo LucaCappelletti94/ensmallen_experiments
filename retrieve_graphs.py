66a8949b43a58a80c44ee5e6ad70f3bb2bc37e90
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-08-04 22:16:42 +0200

    Autonated extraction

--- retrieve_graphs.py
+++ retrieve_graphs.py
@@ -1,11 +1,22 @@
 import compress_json
 from encodeproject import download
 from tqdm.auto import tqdm
+import gzip
+import shutil
+import zipfile
 import os
 
-data = compress_json.load("graphs.json")
-for url in tqdm(data.values(), desc="Downloading graphs"):
-    path = url.split("/")[-1]
-    if os.path.exists(path):
+graphs = compress_json.load("graphs.json")
+for data in tqdm(graphs.values(), desc="Downloading graphs"):
+    if os.path.exists(data["extraction_path"]):
         continue
-    download(url, path)
+    path = data["url"].split("/")[-1]
+    download(data["url"], path)
+    
+    if path.endswith(".gz"):
+        with gzip.open(path, 'rb') as f_in:
+            with open(data["extraction_path"], 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+    elif path.endswith(".zip"):
+        with zipfile.ZipFile(path, 'r') as zip_ref:
+            zip_ref.extract(data["extraction_path"])
