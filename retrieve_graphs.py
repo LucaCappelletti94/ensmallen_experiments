0e4c9d8ecf6fa8197134e017181579a7604f575b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-08-04 21:58:24 +0200

    Updated graph retrieval

--- retrieve_graphs.py
+++ retrieve_graphs.py
@@ -1,7 +1,11 @@
 import compress_json
 from encodeproject import download
 from tqdm.auto import tqdm
+import os
 
 data = compress_json.load("graphs.json")
 for url in tqdm(data.values(), desc="Downloading graphs"):
-    download(url)
+    path = url.split("/")[-1]
+    if os.path.exists(path):
+        continue
+    download(url, path)
