816b13fd7689c6d079b988a3091c13854f0f84a0
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-08-06 17:42:05 +0200

    Added download monarch to utils

--- ensmallen_experiments/utils/__init__.py
+++ ensmallen_experiments/utils/__init__.py
@@ -1,3 +1,4 @@
 """Submodule with tools to make the pipeline more reproducible step by step."""
 from .extract import extract
 from .logger import logger
+from .download_monarch import download_monarch
