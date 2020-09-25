180a2924f0eb5a9f82c2282574c0ede1fdf15778
zom <tommaso.fontana.96@gmail.com>
2020-09-25 19:31:03 +0200

    #2 added a basic test for the measure resource manager

---
+++ tests/test_measure_resources.py
@@ -0,0 +1,19 @@
+"""Test suite to check that the retrieval and sanitization of the graphs works."""
+import os
+from time import sleep
+from ensmallen_experiments import retrieve_graphs
+
+TEST_FILE = "test_file.csv"
+
+def test_retrieve_graphs():
+    """Test that the retrieval of the graph works."""
+    with MeasureResources(TEST_FILE):
+        sleep(0.5)
+        a = list(range(int(1e6)))
+        sleep(0.5)
+        del a
+        sleep(0.5)
+
+    assert os.path.exists(TEST_FILE)
+
+    os.remove(TEST_FILE)
