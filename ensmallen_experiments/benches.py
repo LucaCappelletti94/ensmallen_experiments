58300c8fe1bb8dbd33bd003b846f773183a960c8
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-10-07 18:24:55 +0200

    Added test to run everything

--- ensmallen_experiments/benches.py
+++ ensmallen_experiments/benches.py
@@ -1,6 +1,21 @@
 from .libraries import libraries
-from .utils import build_path_path, get_graph_report
+from .utils import build_path_path, get_graph_report, get_graph_names
 import compress_json
+
+
+def validate_graph_and_library(library: str, graph: str, metadata_path: str):
+    if library not in libraries:
+        raise ValueError(
+            "Given library {} is not currently available for running benchmarks.".format(
+                libraries
+            )
+        )
+    if graph not in get_graph_names(metadata_path):
+        raise ValueError(
+            "Given graph {} is not currently available for running benchmarks.".format(
+                libraries
+            )
+        )
 
 
 def bench_load_graph(library: str, graph: str, metadata_path: str, root: str):
@@ -17,18 +32,8 @@ def bench_load_graph(library: str, graph: str, metadata_path: str, root: str):
     root: str,
         Directory from where to load the graph.
     """
-    if library not in libraries:
-        raise ValueError(
-            "Given library {} is not currently available for running benchmarks.".format(
-                libraries
-            )
-        )
+    validate_graph_and_library(library, graph, metadata_path)
     metadata = compress_json.load(metadata_path)
-    if graph not in metadata:
-        raise ValueError(
-            "Given graph {} is not currently available for running benchmarks.".format(
-                libraries)
-        )
     data = metadata[graph]
     report = get_graph_report(data, root)
 
@@ -36,5 +41,55 @@ def bench_load_graph(library: str, graph: str, metadata_path: str, root: str):
         edge_path=build_path_path(metadata[graph], root),
         nodes_number=int(report["nodes_number"]),
         edges_number=int(report["edges_number"]),
-        has_weights=report["has_weights"]=="true"
+        has_weights=report["has_weights"] == "true"
+    )
+
+
+def bench_random_walks(
+    library: str,
+    graph: str,
+    metadata_path: str,
+    root: str,
+    length: int = 100,
+    iterations: int = 1,
+    p: float = 1.0,
+    q: float = 1.0
+):
+    """Benches executing random walks the given graph using given library.
+
+    Parameters
+    -----------------------
+    library: str,
+        Library to use for the benchmark.
+    graph: str,
+        Graph to use for the benchmark.
+    metadata_path: str,
+        Path from where to load the graph metadata.
+    root: str,
+        Directory from where to load the graph.
+    p: float = 1.0,
+        Inverse of the return weight.
+    q: float = 1.0,
+        Invert of the explore weight.
+    """
+    validate_graph_and_library(library, graph, metadata_path)
+    metadata = compress_json.load(metadata_path)
+    data = metadata[graph]
+    report = get_graph_report(data, root)
+
+    walkers = libraries[library]["execute_walks"]
+
+    graph = walkers["load_graph"](
+        edge_path=build_path_path(metadata[graph], root),
+        nodes_number=int(report["nodes_number"]),
+        edges_number=int(report["edges_number"]),
+        has_weights=report["has_weights"] == "true"
+    )
+
+    walkers["walk"](
+        graph,
+        length=length,
+        iterations=iterations,
+        p=p,
+        q=q
     )
--- tests/test_load_graphs_benches.py
+++ tests/test_load_graphs_benches.py
@@ -1,16 +1,26 @@
 import pytest
-from ensmallen_experiments.utils import get_graph_libraries_names, get_graph_names
-from ensmallen_experiments.benches import bench_load_graph
+from ensmallen_experiments.utils import get_graph_libraries_names, get_walks_libraries_names, get_graph_names
+from ensmallen_experiments.benches import bench_load_graph, bench_random_walks
+from ensmallen_experiments import retrieve_graphs
 from tqdm.auto import tqdm
+import shutil
 
 
 def test_load_graphs_benches():
     """Test that the load graph benches work."""
     metadata_path = "tests/test_graphs.json"
     root = "tests/graphs"
-    for library in tqdm(get_graph_libraries_names(), desc="Testing available libraries"):
-        for graph in tqdm(get_graph_names(metadata_path), desc="Testing available graphs"):
+    retrieve_graphs(metadata_path, root)
+    for graph in tqdm(get_graph_names(metadata_path), desc="Testing available graphs"):
+        for library in tqdm(get_graph_libraries_names(), desc="Testing available graph loading libraries"):
             bench_load_graph(
+                library,
+                graph,
+                metadata_path,
+                root
+            )
+        for library in tqdm(get_walks_libraries_names(), desc="Testing available random walks libraries"):
+            bench_random_walks(
                 library,
                 graph,
                 metadata_path,
@@ -32,3 +42,4 @@ def test_load_graphs_benches():
             metadata_path,
             root
         )
+    shutil.rmtree(root)
