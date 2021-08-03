0ad91ebbad974047d6a6b8ae0d2fbd7969284990
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-08-03 11:30:12 +0200

    Removed already benchmarked libraries from import

--- ensmallen_experiments/libraries/__init__.py
+++ ensmallen_experiments/libraries/__init__.py
@@ -1,29 +1,29 @@
 """Submodule with bench for the various libraries."""
-from .bench_csrgraph import load_graph_csrgraph, execute_walks_csrgraph
+#from .bench_csrgraph import load_graph_csrgraph, execute_walks_csrgraph
 from .bench_ensmallen import (
     load_graph_ensmallen,
     execute_walks_ensmallen,
     load_graph_fast_ensmallen
 )
 #from .bench_graph_embedding import execute_walks_graph_embedding
-from .bench_igraph import load_graph_igraph, execute_first_order_walk_igraph
+#from .bench_igraph import load_graph_igraph, execute_first_order_walk_igraph
 from .bench_networkx import load_graph_networkx
 #from .bench_node2vec import execute_walks_node2vec
 #from .bench_pecanpy import load_graph_pecanpy, execute_walks_pecanpy
 
 
 libraries = {
-    "CSRgraph": {
-        "load_graph": load_graph_csrgraph,
-        "first_order_walk": {
-            "load_graph": "CSRgraph",
-            "walk": execute_walks_csrgraph
-        },
-        "second_order_walk": {
-            "load_graph": "CSRgraph",
-            "walk": execute_walks_csrgraph
-        }
-    },
+    # "CSRgraph": {
+    #     "load_graph": load_graph_csrgraph,
+    #     "first_order_walk": {
+    #         "load_graph": "CSRgraph",
+    #         "walk": execute_walks_csrgraph
+    #     },
+    #     "second_order_walk": {
+    #         "load_graph": "CSRgraph",
+    #         "walk": execute_walks_csrgraph
+    #     }
+    # },
     "EnsmallenGraph": {
         "load_graph": load_graph_ensmallen,
         "first_order_walk": {
@@ -46,13 +46,13 @@ libraries = {
             "walk": execute_walks_ensmallen
         }
     },
-    "iGraph": {
-        "load_graph": load_graph_igraph,
-        "first_order_walk": {
-            "load_graph": "iGraph",
-            "walk": execute_first_order_walk_igraph
-        },
-    },
+    # "iGraph": {
+    #     "load_graph": load_graph_igraph,
+    #     "first_order_walk": {
+    #         "load_graph": "iGraph",
+    #         "walk": execute_first_order_walk_igraph
+    #     },
+    # },
     "Networkx": {
         "load_graph": load_graph_networkx,
     },
